356f97851b586f9a79653bade9da37ded366a0fc
Mehmet-Onduc <mehmetonducx@hotmail.com>
2025-03-20 15:14:28 +0300

    Resp+JS done

---
+++ script.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const menuIcon = document.querySelector("#menu-icon");
+  const navbar = document.querySelector(".navbar");
+
+  menuIcon.onclick = () => {
+    menuIcon.classList.toggle("bx-x");
+    navbar.classList.toggle("active");
+  };
+
+  // Scroll sırasında menüyü kapat
+  window.onscroll = () => {
+    menuIcon.classList.remove("bx-x");
+    navbar.classList.remove("active");
+  };
+});
